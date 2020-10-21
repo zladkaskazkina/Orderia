301856b49eb80a720fab33573888f49ac50c11fa
AdeKriz <AdeKriz@users.noreply.github.com>
2020-10-21 20:49:26 +0200

    homepage update
    not ready yet

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,13 @@
 <template>
-  <v-footer fixed padless app class="info">
-    <v-card flat tile class="text-center info">
-      <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4 error--text" icon>
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
-
-      <v-divider></v-divider>
+  <v-footer fixed padless app class="footer pa-3">
+        <v-card flat tile class="text-center mx-auto">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4 error--text" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
     </v-card>
   </v-footer>
 </template>
@@ -19,8 +17,14 @@ export default {
   name: "Footer",
   data() {
     return {
-      icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+      icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"]
     };
-  },
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.v-footer {
+  background: #ffff !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,61 @@
 <template>
   <v-main class="fill-height">
-    <v-row class="pa-3 fill-height">
-      <v-col md6>
-        <v-card class="pa-5 fill-height" color="primary">
-          <v-list three-line>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>Content here</v-list-item-title>
-                <v-list-item-subtitle
-                  >Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Assumenda itaque obcaecati nostrum in aspernatur praesentium
-                  deserunt. Dolore mollitia cumque esse qui reprehenderit saepe
-                  provident nobis quis, ea vero praesentium
-                  eaque!</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>Other</v-list-item-title>
-                <v-list-item-subtitle
-                  >Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Ipsum ducimus sit eius facilis incidunt facere numquam nisi?
-                  Nihil non doloribus nobis veritatis, architecto asperiores
-                  animi laborum quia delectus tenetur
-                  rerum?</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-card>
+    <v-row class="fill-height" no-gutters>
+      <v-col>
+        <v-sheet class="fill-height supplier-main pa-4">
+          <v-row no-gutters class="px-12 py-10">
+            <p class="home-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Assumenda itaque obcaecati nostrum in aspernatur praesentium
+              deserunt. Dolore mollitia cumque esse qui reprehenderit saepe
+              provident nobis quis, ea vero praesentium eaque!
+            </p>
+          </v-row>
+          <v-row no-gutters>
+            <v-btn flat class="mx-auto" router to="/login">
+              Registrovat se jako dodavatel</v-btn
+            >
+            <!-- VYTVOŘIT REGISTRATION PAGE? -->
+          </v-row>
+          <v-row no-gutters justify="center" class="py-10">
+            <ul>
+              <li>Lorem ipsum dolor sit amet consectetur, adipisicing.</li>
+              <li>Assumenda itaque obcaecati nostrum in aspernatur.</li>
+              <li>Dolore mollitia cumque esse qui reprehenderit.</li>
+            </ul>
+          </v-row>
+        </v-sheet>
       </v-col>
-      <v-col md6>
-        <v-card class="pa-5 fill-height" color="secondary">
-          <v-btn flat router to="/test"> test </v-btn>
-          <v-btn flat router to="/test2"> test2 </v-btn>
-        </v-card>
+      <v-col>
+        <v-sheet class="fill-height buyer-main pa-4">
+          <v-row no-gutters class="px-12 py-10">
+            <p class="home-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Assumenda itaque obcaecati nostrum in aspernatur praesentium
+              deserunt. Dolore mollitia cumque esse qui reprehenderit saepe
+              provident nobis quis, ea vero praesentium eaque!
+            </p>
+          </v-row>
+          <v-row no-gutters>
+            <v-btn flat class="mx-auto" router to="/login">
+              Registrovat se jako odběratel</v-btn
+            >
+            <!-- VYTVOŘIT REGISTRATION PAGE? -->
+          </v-row>
+          <v-row no-gutters justify="center" class="py-10">
+            <ul>
+              <li>Lorem ipsum dolor sit amet consectetur, adipisicing.</li>
+              <li>Assumenda itaque obcaecati nostrum in aspernatur.</li>
+              <li>Dolore mollitia cumque esse qui reprehenderit.</li>
+            </ul>
+          </v-row>
+          <v-row class="ma-10" no-gutters>
+            <div class="mx-auto">
+              <v-btn flat router to="/test"> test </v-btn>
+              <v-btn flat router to="/test2"> test2 </v-btn>
+            </div>
+          </v-row>
+        </v-sheet>
       </v-col>
     </v-row>
   </v-main>
@@ -46,6 +65,21 @@
 // @ is an alias to /src
 export default {
   name: "Home",
-  components: {},
+  components: {}
 };
 </script>
+
+<style lang="scss" scoped>
+.supplier-main {
+  background: url("./../assets/images/background-supplier.jpg") center no-repeat;
+}
+
+.buyer-main {
+  background: url("./../assets/images/background-buyer.jpg") center no-repeat;
+}
+
+.home-text {
+  text-align: center;
+  color: #431c5d;
+}
+</style>
