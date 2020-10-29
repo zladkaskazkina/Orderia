56692b4d6e45cab184b40ec256960c8f4976651a
AdeKriz <AdeKriz@users.noreply.github.com>
2020-10-29 17:48:40 +0100

    mensi oprava main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router/router";
-import store from "./store";
+import { store } from "./store";
 import vuetify from "./plugins/vuetify";
 import Vuelidate from 'vuelidate';
 import axios from 'axios';
